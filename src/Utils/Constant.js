2950a60879086077ff1f67fb8140fd085d47b8b7
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2022-07-22 22:35:25 +0700

    new endpoint 😎

--- src/Utils/Constant.js
+++ src/Utils/Constant.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://mendoan.prasetyonh.my.id";
+export const API_URL = "https://server-mendoan.prasetyonh.my.id";
 export const FAKEAPI_URL = "https://db-mendoan.herokuapp.com";
