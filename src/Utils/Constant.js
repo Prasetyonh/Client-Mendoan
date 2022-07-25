fc6147d856165ac2e3b949c1f383921d50dbbb0f
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2022-07-26 04:16:45 +0700

    update url fake-api

--- src/Utils/Constant.js
+++ src/Utils/Constant.js
@@ -1,2 +1,2 @@
 export const API_URL = "https://api-mendoans247.prasetyonh.my.id";
-export const FAKEAPI_URL = "http://20.11.64.132:2472";
+export const FAKEAPI_URL = "https://fake-api.prasetyonh.my.id";
