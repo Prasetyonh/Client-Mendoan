6377d929a6fcd8e2ad23ad2b2646f4fdc80b0b5e
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2022-07-26 15:53:28 +0700

    update url

--- src/Pages/user/Register.jsx
+++ src/Pages/user/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import validator from "validator";
 import axios from "axios";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import Swal from "sweetalert2";
 import { Button } from "react-bootstrap";
 
